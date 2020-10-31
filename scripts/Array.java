f38ea85e2b5a35ca1ef5ec4b88cc00974aaa68b6
EnTxSARCaSTIC <73741546+EnTxSARCaSTIC@users.noreply.github.com>
2020-10-31 16:54:11 +0530

    Create Array.java
    It's an array code pls accept my pull request pls pls sir pls

---
+++ scripts/Array.java
@@ -0,0 +1,5 @@
+int box; //to track the current box the player is on at the moment
+char a[3][3]; //array to hold the actual values that player enter while playing
+int navigate(char a[3][3], int box, int player, int key);
+// to handle key presses and update the current box the player is on
+// and to enter the move in to the box when player presses Enter.
